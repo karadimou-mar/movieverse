2c4b934cea5dd38bd19db8dc626583f301159d7b
karadimou-mar <karadimou.mar@gmail.com>
2021-09-01 01:43:36 +0300

    searchMovie: Added logic for searching a movie, both typing logic and "simple" one.

--- app/src/main/java/com/example/movieverse/HomeScreen.kt
+++ app/src/main/java/com/example/movieverse/HomeScreen.kt
@@ -6,16 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.movieverse.databinding.HomeScreenBinding
 import com.example.movieverse.net.NetworkResponse
+import com.example.movieverse.util.callBackWhileTyping
 import com.example.movieverse.viewmodel.SearchViewModelUser
 import com.example.movieverse.viewmodel.activitySearchViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
 
+@FlowPreview
 class HomeScreen : Fragment(), SearchViewModelUser {
 
     private var _binding: HomeScreenBinding? = null
     private val binding
         get() = _binding!!
+
+    private var searchJob: Job? = null
 
     override val searchViewModel by activitySearchViewModel()
 
@@ -28,17 +37,30 @@ class HomeScreen : Fragment(), SearchViewModelUser {
         return binding.root
     }
 
+    @ExperimentalCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         subscribeObservers()
-        searchMovie("harry")
+
+        // search on search icon click
+        binding.searchSection.searchIcon.setOnClickListener {
+            searchMovie()
+        }
+        //search also while typing
+        //TODO: keep both?
+        //searchMovieWhileTyping()
     }
 
     private fun subscribeObservers() {
         searchViewModel.searchMovieResult.observe(viewLifecycleOwner, {
             when (val response1 = it) {
-                is NetworkResponse.Success -> Log.d(TAG, "Success: ${response1.body.results}")
+                is NetworkResponse.Success -> {
+                    Log.d(TAG, "Success: ${response1.body.results}")
+                    for (i in response1.body.results) {
+                        binding.dummyTv.append(i.title + "   ")
+                    }
+                }
                 is NetworkResponse.ApiError -> Log.d(
                     TAG,
                     "ApiError: statusCode: ${response1.body.statusCode} , statusMsg: ${response1.body.statusMsg}"
@@ -49,13 +71,25 @@ class HomeScreen : Fragment(), SearchViewModelUser {
         })
     }
 
-    private fun searchMovie(query: String) {
-        searchViewModel.searchMovie(query)
+    @ExperimentalCoroutinesApi
+    @FlowPreview
+    private fun searchMovieWhileTyping() {
+        searchJob = binding.searchSection.searchInput.callBackWhileTyping {
+            it?.let { textToSearch ->
+                searchViewModel.searchMovie(query = textToSearch.toString())
+            }
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun searchMovie() {
+        val textToSearch = binding.searchSection.searchInput.text.toString()
+        searchViewModel.searchMovie(query = textToSearch)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        searchJob?.cancel()
     }
 
     companion object {
--- app/src/main/java/com/example/movieverse/util/EditText.kt
+++ app/src/main/java/com/example/movieverse/util/EditText.kt
@@ -1,4 +1,42 @@
 package com.example.movieverse.util
 
-class EditText {
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
+
+@ExperimentalCoroutinesApi
+private fun EditText.textChanges(): Flow<CharSequence?> {
+    return callbackFlow {
+        val listener = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                trySend(p0).isSuccess
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+        }
+        addTextChangedListener(listener)
+        awaitClose {
+            removeTextChangedListener(listener)
+        }
+    }
 }
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+fun EditText.callBackWhileTyping(
+    delay: Long = 1000L,
+    callback: (CharSequence?) -> Unit
+): Flow<CharSequence?> =
+    textChanges().debounce(delay).onEach {
+        callback(it)
+    }
